db4f5b08151da0ef1668a4753b603951b615a2ee
shaikh_noman <shaikhmohdnoman612@gmail.com>
2024-03-04 15:05:13 +0530

    Minor Fixes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Posts from "./posts/page";
 
 export default function Home() {
   return (
-    <main>
+    <main className="p-10">
       <Posts />
       <h1 className="text-md p-4">Instaling Redux toolit in Next.js 14</h1>
     </main>
